1d52879e786dc4c58775566780de1c496c17ae45
“slaptashka” <slaptashkaa@gmail.com>
2024-01-25 19:55:35 +0200

    add postgres

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_PEMP66rSPVPMjzveApdIQ1DTcaK3/X9yTMlawDo",
+      apiToken: "HjBS49Y1csiSUT3ih2eNfpEv",
+      appFilter: "strapi-test",
+      teamFilter: "itslapsu",
+      roles: ["strapi-super-admin"],
+    },
   },
 });
